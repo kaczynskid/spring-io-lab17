22288de8708d5750680f1bf380385c53b045b3c5
Darek Kaczyński <kaczynskid@gmail.com>
2018-11-19 14:42:18 +0100

    Extracted marketing module

---
+++ marketing/src/main/java/io/spring/lab/marketing/MarketingApplication.java
@@ -0,0 +1,12 @@
+package io.spring.lab.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingApplication.class, args);
+	}
+}
