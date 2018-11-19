367e156b35640611645aa5b7f6c5db39fa44af29
Darek Kaczyński <kaczynskid@gmail.com>
2018-11-19 12:29:25 +0100

    Added demo with simple test

---
+++ demo/src/main/java/io/spring/lab/demo/DemoApplication.java
@@ -0,0 +1,41 @@
+package io.spring.lab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingController {
+
+	@GetMapping("/greet/{name}")
+	Greeting sayHello(@PathVariable String name) {
+		return new Greeting(String.format("Hi %s", name));
+	}
+}
+
+@lombok.Value
+class Greeting {
+
+	private String message;
+
+	@JsonCreator
+	public static Greeting of(@JsonProperty("message") String message) {
+		return new Greeting(message);
+	}
+}
