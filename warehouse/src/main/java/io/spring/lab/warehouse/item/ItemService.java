f9513a2f5ed3308f459046160ca5473a45f35d1c
Darek Kaczyński <kaczynskid@gmail.com>
2018-11-20 13:17:05 +0100

    Added metrics reporting to warehouse

--- warehouse/src/main/java/io/spring/lab/warehouse/item/ItemService.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/item/ItemService.java
@@ -1,10 +1,14 @@
 package io.spring.lab.warehouse.item;
 
+import java.util.Arrays;
 import java.util.List;
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.Validate;
 import org.springframework.stereotype.Component;
 
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tag;
 import lombok.AllArgsConstructor;
 
 @Component
@@ -13,16 +17,24 @@ public class ItemService {
 
 	private final ItemRepository items;
 
+	@PostConstruct
+	public void init() {
+		Metrics.gauge("warehouse.items.count", tagsFor("read"), items, ItemRepository::count);
+	}
+
 	public Item findOne(long id) {
+		increment(id + ".get", "read");
 		return items.findOne(id)
 				.orElseThrow(() -> new ItemNotFound(id));
 	}
 
 	public List<Item> findAll() {
+		increment("get", "read");
 		return items.findAll();
 	}
 
 	public Item create(Item item) {
+		increment("create", "create");
 		return items.save(item);
 	}
 
@@ -31,6 +43,7 @@ public class ItemService {
 
 		Item item = findOne(changes.getId());
 		item.update(changes);
+		increment(item.getId() + ".update", "update");
 		return items.save(item);
 	}
 
@@ -39,8 +52,20 @@ public class ItemService {
 
 		Item item = findOne(changes.getId());
 		item.updateStock(changes);
-
+		increment(item.getId() + ".stock.update", "update");
 		return items.save(item);
+	}
+
+	private void increment(String name, String operation) {
+		Metrics.counter("warehouse.items." + name, tagsFor(operation)).increment();
+	}
+
+	private static Iterable<Tag> tagsFor(String operation) {
+		return Arrays.asList(
+				Tag.of("service", "warehouse"),
+				Tag.of("domain", "item"),
+				Tag.of("operation", operation)
+		);
 	}
 }
 
