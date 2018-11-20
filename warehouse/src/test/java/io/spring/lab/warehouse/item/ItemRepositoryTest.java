265ae7c039be64b02c2c06301cd0d01715051204
Darek Kaczyński <kaczynskid@gmail.com>
2018-11-20 09:58:44 +0100

    Added repository test

--- warehouse/src/test/java/io/spring/lab/warehouse/item/ItemRepositoryTest.java
+++ warehouse/src/test/java/io/spring/lab/warehouse/item/ItemRepositoryTest.java
@@ -1,19 +1,32 @@
 package io.spring.lab.warehouse.item;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.spring.lab.warehouse.WarehousePersistenceConfig;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.context.annotation.FilterType.ANNOTATION;
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataJpaTest(includeFilters = {
+        @Filter(type = ASSIGNABLE_TYPE, classes = WarehousePersistenceConfig.class),
+        @Filter(type = ANNOTATION, classes = Repository.class)
+})
 public class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository items;
 
     @Autowired
     TestEntityManager jpa;
@@ -35,4 +48,19 @@ public class ItemRepositoryTest {
         assertThat(item.getCount()).isEqualTo(99);
         assertThat(item.getPrice()).isEqualTo(BigDecimal.valueOf(333000, 4));
     }
+
+    @Test
+    public void shouldFindExistingItem() {
+        // when
+        Optional<Item> theOne = items.findOne(1L);
+
+        // then
+        assertThat(theOne).isPresent();
+        theOne.ifPresent(item -> {
+            assertThat(item.getName()).isEqualTo("A");
+            assertThat(item.getCount()).isEqualTo(100);
+            assertThat(item.getPrice()).isEqualTo(BigDecimal.valueOf(400000, 4));
+        });
+    }
+
 }
