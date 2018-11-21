32a81efe51cde1352b634711d177512d1c789583
Darek Kaczyński <kaczynskid@gmail.com>
2018-11-21 14:26:15 +0100

    Added base class for tests

--- demo/src/test/java/io/spring/lab/demo/DemoApplicationTests.java
+++ demo/src/test/java/io/spring/lab/demo/DemoApplicationTests.java
@@ -1,19 +1,16 @@
 package io.spring.lab.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(properties = {
 		"greet.template=whatever"
 })
-public class DemoApplicationTests {
+public class DemoApplicationTests extends SpringTestBase {
 
 	@Autowired
 	ApplicationContext context;
--- demo/src/test/java/io/spring/lab/demo/GreetingControllerTest.java
+++ demo/src/test/java/io/spring/lab/demo/GreetingControllerTest.java
@@ -1,20 +1,17 @@
 package io.spring.lab.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = {
         "greet.template=Hello %s"
 })
-public class GreetingControllerTest {
+public class GreetingControllerTest extends SpringTestBase {
 
     @Autowired
     TestRestTemplate rest;
